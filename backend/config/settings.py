0923bf08eea23c5d360daaa5924201ebfe7e6af8
Fayaz <shaikfayazfz64@gmail.com>
2026-04-25 23:48:24 -0500

    failed to deploy front end to vercel

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,7 +37,7 @@ class Settings:
             "http://127.0.0.1:3000",
             "http://127.0.0.1:5173",
             "http://127.0.0.1:5174",
-            "https://ship-smart-ai.vercel.app/",
+            "https://ship-smart-ai.vercel.app",
         ]
 
 settings = Settings()
